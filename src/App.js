9d85a84da0f9bead22aeb62a91872d4f820b7259
bergxp <berg215@hotmail.com>
2025-07-13 22:16:24 -0300

    rollback

--- src/App.js
+++ src/App.js
@@ -5,20 +5,10 @@ import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Testemonial from './Components/Testemonial';
 import Work from './Components/Work';
-import { useEffect } from 'react';
-import { initFacebookPixel } from './pixel';
-import { useLocation } from 'react-router-dom';
-import ReactPixel from 'react-facebook-pixel';
+
 
 function App() {
-  const location = useLocation();
-  useEffect(() => {
-    initFacebookPixel();
-  }, []);
-
-  useEffect(() => {
-    ReactPixel.pageView();
-  }, [location]);
+ 
   return (
     <div className="App">
       <Home/>
